48d882b9241cfbf3d574e88fa4a168c1fd521de7
Gabriel <gcacars@gmail.com>
2015-05-10 10:44:36 -0300

    Router, Click callback, Offset and Show close opts

--- client/s-alert.js
+++ client/s-alert.js
@@ -6,35 +6,50 @@ var conditionSet = function (self, msg, condition, customSettings) {
     var effects = ['jelly', 'genie', 'stackslide', 'scale', 'slide', 'flip', 'bouncyflip'];
     var currentEffect;
     var sAlertId;
+    
     if (!_.isObject(customSettings)) {
         customSettings = {};
     }
+    
     if (_.isObject(msg) && _.isString(condition)) {
         settings = _.extend(settings, self.settings, msg, {condition: condition}, customSettings);
     }
+    
     if (_.isString(msg) && _.isString(condition)) {
         settings = _.extend(settings, self.settings, {message: msg}, {condition: condition}, customSettings);
     }
+    
     currentEffect = settings && settings.effect;
     if (_.contains(effects, currentEffect) && !Package['juliancwirko:s-alert-' + currentEffect] && typeof console !== 'undefined') {
         console.info('Install "' + currentEffect + '" effect by running "meteor add juliancwirko:s-alert-' + currentEffect + '"');
     }
-    if (_.isObject(settings)) {
-        sAlertId = sAlert.collection.insert(settings);
+    
+    if (_.isObject( settings )) {
+        sAlertId = sAlert.collection.insert( settings );
+        
+        if (_.isFunction( settings.onClick )){
+            sAlert.callbacks[ 'call_' + sAlertId ] = settings.onClick;
+        }
     }
+    
     return sAlertId;
 };
 
 var EVENTS = 'webkitAnimationEnd oAnimationEnd animationEnd msAnimationEnd animationend';
+
 var sAlertClose = function (alertId) {
     var closingTimeout;
+    
     if (document.hidden || document.webkitHidden || $('.s-alert-box').css('animationName') === 'none') {
         sAlert.collection.remove(alertId);
+        
     } else {
         $('.s-alert-box#' + alertId).removeClass('s-alert-show');
+        
         closingTimeout = Meteor.setTimeout(function () {
             $('.s-alert-box#' + alertId).addClass('s-alert-hide');
         }, 100);
+        
         $('.s-alert-box#' + alertId).off(EVENTS);
         $('.s-alert-box#' + alertId).on(EVENTS, function () {
             $(this).hide();
@@ -50,24 +65,38 @@ sAlert = {
         effect: '',
         position: 'right-top',
         timeout: 5000,
-        html: false
+        html: false,
+        showClose: true,
+        router: true,
+        offset: {
+            top: 0,
+            bottom: 0
+        },
+        onClick: {}
     },
+    
     config: function (configObj) {
         var self = this;
-        if (_.isObject(configObj)) {
-            self.settings = _.extend(self.settings, configObj);
+        
+        if (_.isObject( configObj )) {
+            self.settings = _.extend( self.settings, configObj );
+            
         } else {
-            throw new Meteor.Error(400, 'Config must be an object!');
+            throw new Meteor.Error(400, 'sAlert: Config must be an object!');
         }
     },
+    
     closeAll: function () {
         sAlert.collection.remove({});
     },
+    
     close: function (id) {
-        if (_.isString(id)) {
-            sAlertClose(id);
+        if (_.isString( id )) {
+            var next = _.isFunction( this.settings.onClick ) ? this.settings.onClick() : true;
+            if (next || next === undefined) sAlertClose( id );
         }
     },
+    
     info: function (msg, customSettings) {
         return conditionSet(this, msg, 'info', customSettings);
     },
@@ -79,20 +108,28 @@ sAlert = {
     },
     warning: function (msg, customSettings) {
         return conditionSet(this, msg, 'warning', customSettings);
-    }
+    },
+    
+    // Alert callbacks
+    callbacks: []
 };
 
 // routers clean
 Meteor.startup(function () {
     if (typeof Iron !== 'undefined' && typeof Router !== 'undefined') {
-        Router.onStop(function () {
-            sAlert.collection.remove({});
-        });
+        if (sAlert.settings.router){
+            Router.onStop(function () {
+                sAlert.collection.remove({});
+            });
+        }
     }
+    
     if (typeof FlowRouter !== 'undefined') {
-        FlowRouter.middleware(function (path, next) {
-            sAlert.collection.remove({});
-            next();
-        });
+        if (sAlert.settings.router){
+            FlowRouter.middleware(function (path, next) {
+                sAlert.collection.remove({});
+                next();
+            });
+        }
     }
 });
